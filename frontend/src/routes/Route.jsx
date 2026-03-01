ac6dde83977ee19b0eb3fb5f210847ed3d7ef9f6
SATYA PREM <167444046+SATYA-PREM@users.noreply.github.com>
2026-03-01 16:51:33 +0530

    Update Route.jsx

--- frontend/src/routes/Route.jsx
+++ frontend/src/routes/Route.jsx
@@ -42,7 +42,6 @@ function Endpoint() {
           <Route element={<ProtectedRoute />}>
             <Route path="/govt" element={<Govt />} />
             <Route path="/company" element={<Company />} />
-            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/" element={<Home />} />
             <Route path="/PatientDashboard" element={<PatientDashboard />} />
